7cba1f92771cbf08fc15c46167f5d52271b6f623
Joao Peixoto <joao.hartimer@gmail.com>
2021-12-10 16:58:34 +0000

    Day 15

--- 2015/day15/code.go
+++ 2015/day15/code.go
@@ -57,15 +57,27 @@ func extractNumber(s string) (int, error) {
 	return strconv.Atoi(parts[1])
 }
 
-func (i ingredient) score(teaspoons int) int {
-	return i.capacity*teaspoons +
-		i.durability*teaspoons +
-		i.flavor*teaspoons +
-		i.texture*teaspoons +
-		i.calories*teaspoons
+func (i ingredient) dosage(teaspoons int) dose {
+	r := i
+	r.capacity = r.capacity * teaspoons
+	r.durability = r.durability * teaspoons
+	r.flavor = r.flavor * teaspoons
+	r.texture = r.texture * teaspoons
+	r.calories = r.calories * teaspoons
+	return dose(r)
 }
 
+type dose ingredient
+
 func Solve1(inputFilePath string) (int, error) {
+	return solve(inputFilePath, false)
+}
+
+func Solve2(inputFilePath string) (int, error) {
+	return solve(inputFilePath, true)
+}
+
+func solve(inputFilePath string, caresAboutCalories bool) (int, error) {
 	var ingredients []ingredient
 	for fileLine := range helper.FileLineReader(inputFilePath) {
 		i, err := newIngredient(fileLine)
@@ -76,23 +88,52 @@ func Solve1(inputFilePath string) (int, error) {
 	}
 	availableTeaspoons := 100
 
-	return bestScore(availableTeaspoons, ingredients), nil
+	return bestScore(availableTeaspoons, []dose{}, ingredients, caresAboutCalories), nil
 }
 
-func bestScore(availableTeaspoons int, ingredients []ingredient) int {
-	if len(ingredients) == 0 || availableTeaspoons == 0 {
-		return 1
+func bestScore(availableTeaspoons int, usedIngredients []dose, remainingIngredients []ingredient, caresAboutCalories bool) int {
+	if len(remainingIngredients) == 0 || availableTeaspoons == 0 {
+		capacity := 0
+		durability := 0
+		flavor := 0
+		texture := 0
+		calories := 0
+		for _, d := range usedIngredients {
+			capacity += d.capacity
+			durability += d.durability
+			flavor += d.flavor
+			texture += d.texture
+			calories += d.calories
+		}
+		if caresAboutCalories && calories != 500 {
+			return 0
+		}
+		return maxInt(0, capacity) *
+			maxInt(0, durability) *
+			maxInt(0, flavor) *
+			maxInt(0, texture)
 	}
 
-	currentIngredient := ingredients[0]
+	currentIngredient := remainingIngredients[0]
 
 	best := -1
-	for i := 0; i < availableTeaspoons; i++ {
-		partial := currentIngredient.score(i)
-		partial *= bestScore(availableTeaspoons-i, ingredients[1:])
+	i := 0
+	if len(remainingIngredients) == 1 {
+		i = availableTeaspoons
+	}
+	for ; i <= availableTeaspoons; i++ {
+		d := currentIngredient.dosage(i)
+		partial := bestScore(availableTeaspoons-i, append(usedIngredients, d), remainingIngredients[1:], caresAboutCalories)
 		if partial > best {
 			best = partial
 		}
 	}
 	return best
+}
+
+func maxInt(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
 }
--- 2015/day15/code_test.go
+++ 2015/day15/code_test.go
@@ -9,7 +9,15 @@ import (
 func TestSolve1(t *testing.T) {
 	inputs := map[string]int{
 		"./input_test.txt": 62842880,
-		// "./input.txt":      664,
+		"./input.txt":      18965440,
 	}
 	testhelper.Runner(t, day15.Solve1, inputs)
+}
+
+func TestSolve2(t *testing.T) {
+	inputs := map[string]int{
+		"./input_test.txt": 57600000,
+		"./input.txt":      15862900,
+	}
+	testhelper.Runner(t, day15.Solve2, inputs)
 }
